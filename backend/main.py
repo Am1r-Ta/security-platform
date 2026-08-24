32acd9db1f524dceedc02155dd1183d491fd9c1c
Am1r-Ta <1amirrezataheri1@gmail.com>
2026-08-24 20:17:46 +0330

    Add system status API

--- backend/main.py
+++ backend/main.py
@@ -3,3 +3,11 @@ app = FastAPI()
 @app.get("/health")
 def health_check():
     return{"stutus": "ok"}
+@app.get("/info")
+def info_check():
+    return{"project": "Security Platform","version": "0.1.0"}
+@app.get("/api/v1/system/status")
+def path_check():
+    return{"status": "online",
+        "service": "security-platform-backend",
+        "version": "0.1.0"}
